6cd43fea0df6c5b5d30184507b8ae9250d24835b
Precious-12 <morganamay740@gmail.com>
2022-07-15 16:32:11 -1200

    Adding my Axlr8 Assignment.py

---
+++ Axlr8 Assignment.py.py
@@ -0,0 +1,7 @@
+Name=input("Enter your name: ")
+Age=int(input("Enter your age: "))
+def myfunc():
+    current_year=2022
+    x=current_year-Age
+    print(f"Hello, {Name}. You are born in {x}")
+myfunc()
